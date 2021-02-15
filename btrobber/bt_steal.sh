f5be13836cf5454e0893bee2d7e856a04ee03f9e
Ian Dinwoodie <dinwoodieian@gmail.com>
2021-02-15 23:54:20 +0000

    Cleaned up bluetooth steal script.

--- btrobber/bt_steal.sh
+++ btrobber/bt_steal.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
-# Disable BT on remote machine and enable it on local machine, so KB/Mouse reconnect to local machine.
-#ensure local bluetooth is off
-echo $(/usr/local/bin/blueutil off)
-#disable imac bluetooth
-ssh neon '/usr/local/bin/blueutil -p 0'
-#enable local bluetooth
-echo $(/usr/local/bin/blueutil on)
-# record the new status
-echo "echo \"on\"" > ~/bt_status.sh
+
+# Disable bluetooth on the remove machine and enable it on the local machine so
+# that connections to bluetooth devices from the remove machine are dropped and
+# then reestablished on the local machine.
+# Usage: ./bt_release <remote-host>
+function main
+{
+  local remote_host="$1"
+  # Ensure local bluetooth is off.
+  echo $(/usr/local/bin/blueutil off)
+  # Disable remote bluetooth.
+  ssh "${remote_host}" '/usr/local/bin/blueutil -p 0'
+  # Reenable local bluetooth
+  echo $(/usr/local/bin/blueutil on)
+}
+
+main "$@"
