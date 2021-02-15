168eafacd5c3f973efa69b4f72cb26a89f860205
Ian Dinwoodie <dinwoodieian@gmail.com>
2021-02-15 23:47:22 +0000

    Cleaned up bluetooth release script.

--- btrobber/bt_release.sh
+++ btrobber/bt_release.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
-# Disable BT on local machine and enable it on remote machine, so KB/Mouse reconnect to remote machine.
-# ensure local bluetooth is off
-echo $(/usr/local/bin/blueutil off)
-# enable imac bluetooth
-ssh neon '/usr/local/bin/blueutil -p 1'
-# wait 2 minutes for keyboard to reconnect with remote machine
-echo $(/bin/sleep 30)
-# enable local bluetooth
-echo $(/usr/local/bin/blueutil on)
-# record the new status
-echo "echo \"off\"" > ~/bt_status.sh
+
+# Disable bluetooth on local machine and enable it on remote machine so that
+# connections to bluetooth devices from the local machine are dropped and
+# then reestablished on the remote machine.
+# Usage: ./bt_release <remote-host>
+function main
+{
+  local remote_host="$1"
+  # Ensure local bluetooth is off.
+  echo $(/usr/local/bin/blueutil off)
+  # Enable remote bluetooth.
+  ssh "${remote_host}" '/usr/local/bin/blueutil -p 1'
+  # Wait 30 seconds for devices to connect with remote machine.
+  echo $(/bin/sleep 30)
+  # Reenable local bluetooth.
+  echo $(/usr/local/bin/blueutil on)
+}
+
+main "$@"
